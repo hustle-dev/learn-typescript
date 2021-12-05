e5d914220c723a4730640596a8d2f4f8ad688638
hustle-dev <dlwoabsdk@gmail.com>
2021-12-05 21:06:33 +0900

    feat: typescirpt introduction and background
    - why typescript?
    - typescript advantage 1
    - typescript advantage 2
    - js coding method like ts

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,5 +1,5 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = 'https://jsonplaceholder.typicode.com/users';
 
 // dom
 var username = document.querySelector('#username');
@@ -9,13 +9,32 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {object} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then(function (response) {
+  response.address;
+});
+
 function startApp() {
-  axios
-    .get(url)
+  fetchUser()
     .then(function (response) {
-      console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      username.innerText = user[0].name;
+      email.innerText = user[0].email;
+      address.innerText = user[0].address.street;
     })
     .catch(function (error) {
       console.log(error);
---
+++ why-ts/sample.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * @param {number} a
+ * @param {number} b
+ */
+const add = (a, b) => {
+  return a + b;
+};
+
+add(10, '20');
