ddda6f65967289277a592b000e18a90436b0fc93
hustle-dev <dlwoabsdk@gmail.com>
2021-12-11 19:06:33 +0900

    feat: TS 클래스와 JS 프로토타입
    - 클래스 소개
    - JS의 프로토타입
    - 프로토타입과 클래스의 관계
    - 타입스크립트의 클래스 문법

---
+++ class-note/class.js
@@ -0,0 +1,17 @@
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+// const capt = new Person('캡틴', 100);
+
+// class Person {
+//   // 클래스 로직
+//   constructor(name, age) {
+//     console.log('생성 되었습니다.');
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+// const JM = new Person('정민', 25); // 생성 되었습니다.
+// console.log(JM);
